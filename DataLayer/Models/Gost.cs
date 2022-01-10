1b02dcce1e6edf9cedf7b2a028385a6819a04fd9
LAPTOP-VU1H8TCL\David <jasovicdavid22@gmil.com>
2022-01-10 23:48:11 +0100

    Dodao sam folder Models i klasu Gost.

---
+++ DataLayer/Models/Gost.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    public class Gost
+    {
+
+        public string BrojLicneKarte { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string BrojTelefona { get; set; }
+        public int BrojSobe { get; set; }
+
+        public Gost()
+        {
+
+        }
+
+        public Gost(string BrojLicneKarte, string Ime, string Prezime, string BrojTelefona, int BrojSobe)
+        {
+            this.BrojLicneKarte = BrojLicneKarte;
+            this.Ime = Ime;
+            this.Prezime = Prezime;
+            this.BrojTelefona = BrojTelefona;
+            this.BrojSobe = BrojSobe;
+        }
+
+        public override string ToString()
+        {
+            return $"Broj lične karte: {BrojLicneKarte}, Ime: {Ime}, Prezime: {Prezime}, Broj telefona: {BrojTelefona}, Broj sobe: {BrojSobe}";
+        }
+    }
+}
